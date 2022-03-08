870906966727184f030870467e1fec13d14d3be1
josemiguel02 <jeffrey_m@hotmail.es>
2022-03-08 03:33:46 -0500

    update services

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,23 +1,21 @@
 import axios from 'axios'
 import { Camera } from '../interfaces'
-import { BASE_URL } from '../utils/base-url'
+import { API_URL } from '../utils/base-url'
 
-export const addCamera = async (newCamera: Camera) => {
+export const addCamera = async (newCamera: Camera, image: File) => {
   const result = { dataCamera: {} as Camera, error: false }
 
+  const { name, model, brand, connection_type, price } = newCamera
+  const FD = new FormData()
+  FD.append('name', name)
+  FD.append('model', model)
+  FD.append('brand', brand)
+  FD.append('connection_type', connection_type)
+  FD.append('price', price as string)
+  FD.append('img', image)
+
   try {
-    const { name, model, brand, connection_type, price } = newCamera
-
-    const { data } = await axios.post(`${BASE_URL}/add`, {
-      name,
-      model,
-      brand,
-      price,
-      connection_type,
-      image:
-        'https://cdn.computerhoy.com/sites/navi.axelspringer.es/public/styles/480/public/media/image/2020/03/mejores-camaras-vigilancia-ip-puedes-comprar-1887273.jpg?itok=9-0Vaxef'
-    })
-
+    const { data } = await axios.post(`${API_URL}/add`, FD)
     result.dataCamera = data
   } catch (e) {
     result.error = true
@@ -27,20 +25,24 @@ export const addCamera = async (newCamera: Camera) => {
   return result
 }
 
-export const editCamera = async (id: string, cameraEdited: Camera) => {
+export const editCamera = async (
+  id: string,
+  cameraEdited: Camera,
+  image: File
+) => {
   const result = { dataCamera: {} as Camera, error: false }
 
   const { name, model, brand, connection_type, price } = cameraEdited
+  const FD = new FormData()
+  FD.append('name', name)
+  FD.append('model', model)
+  FD.append('brand', brand)
+  FD.append('connection_type', connection_type)
+  FD.append('price', price as string)
+  FD.append('img', image)
 
   try {
-    const { data } = await axios.put(`${BASE_URL}/edit/${id}`, {
-      name,
-      model,
-      brand,
-      price,
-      connection_type
-    })
-
+    const { data } = await axios.put(`${API_URL}/edit/${id}`, FD)
     result.dataCamera = data
   } catch (e) {
     result.error = true
@@ -54,7 +56,7 @@ export const deleteCamera = async (id: string) => {
   const result = { error: false }
 
   try {
-    await axios.delete(`${BASE_URL}/delete/${id}`)
+    await axios.delete(`${API_URL}/delete/${id}`)
   } catch (e) {
     result.error = true
     console.error(e)
