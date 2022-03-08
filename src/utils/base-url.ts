26d9670e39a2c52679fc1cab79667bd9c091ab92
josemiguel02 <jeffrey_m@hotmail.es>
2022-03-08 04:05:41 -0500

    edit api url

--- src/utils/base-url.ts
+++ src/utils/base-url.ts
@@ -1,9 +1,7 @@
 const __DEV__ = process.env.NODE_ENV !== 'production'
 
-export const API_URL = __DEV__
-  ? 'http://localhost:5000/api/cameras'
-  : 'https://cameras-api.onrender.com/api/cameras'
-
 export const BASE_URL = __DEV__
   ? 'http://localhost:5000'
   : 'https://cameras-api.onrender.com'
+
+export const API_URL = `${BASE_URL}/api/cameras`
